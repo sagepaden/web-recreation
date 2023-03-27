d017091eae51cdc4e3f45708df0d3cd25df11c5f
sagepaden <sagepaden@gmail.com>
2023-03-27 16:05:43 -0400

    added second h1 and img to scripts file

--- js/scripts.js
+++ js/scripts.js
@@ -8,21 +8,29 @@ window.onload = function() {
   const body = document.querySelector("body");
   console.log(body);
 
-  const hH1 = document.createElement("h1")
-  hH1.append("Webpage Recreation Practice")
-  body.append(hH1);
-  console.log(hH1);
+  const h1_1 = document.createElement("h1")
+  h1_1.append("Webpage Recreation Practice")
+  body.append(h1_1);
+  console.log(h1_1);
 
   const p = document.createElement("p")
   p.append("The HTML of this webpage was created with JavaScript.")
   body.append(p);
   console.log(p);
 
-  const img = document.createElement("img")
-  img.setAttribute("src", 'https://cdn.download.ams.birds.cornell.edu/api/v1/asset/202984001/900')
-  img.setAttribute("alt", "This is an image of a Multicolored Tanager bird")
-  img.style.width = "50%"
-  body.append(img);
+  const img_1 = document.createElement("img")
+  img_1.setAttribute("src", 'https://cdn.download.ams.birds.cornell.edu/api/v1/asset/202984001/900')
+  img_1.setAttribute("alt", "This is an image of a Multicolored Tanager bird")
+  img_1.style.width = "50%"
+  img_1.before(p)
+  body.append(img_1);
+
+  const h1_2 = document.createElement("h1")
+  h1_2.append("Facts about the Multicolored Tanager")
+  h1_2.after(img_1)
+  body.append(h1_2)
+  console.log(h1_2);
+
 
 }  
 
